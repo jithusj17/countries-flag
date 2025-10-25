3d0ba24fd06501ecd03c9131f262c74d53a7edc5
Jithu SJ <jithusj169@gmail.com>
2025-10-25 14:31:56 +0530

    added

--- src/assets/Countries.jsx
+++ src/assets/Countries.jsx
@@ -16,7 +16,7 @@ export default function Countries() {
         const res = await axios.get("https://xcountries-backend.labs.crio.do/all");
         setData(res.data);
       } catch (err) {
-        console.error("Error feteching data:", err);
+        console.error("Error fetching data:", err);
         setError("Failed to fetch countries. Please try again later."); 
       } finally {
         setLoading(false);
